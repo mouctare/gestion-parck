5f2a16fb8d5958095e22f64efd002140507b4af6
mouctare <mouctard78@gmail.com>
2021-06-04 21:25:05 +0200

    recup des continent de l'animal avec une req non optimisé

--- controllers/front/API.controller.php
+++ controllers/front/API.controller.php
@@ -11,16 +11,59 @@ class APIController {
     }
     public function getAnimaux(){
         $animaux = $this->apiManager->getDBAnimaux();
+        Model::sendJSON($this->formatDataLignesAnimaux($animaux));
        
     }
 
     public function getAnimal($idAnimal){
         $lignesAnimal = $this->apiManager->getDBAnimal($idAnimal);
-        echo "<pre>";
-        print_r($lignesAnimal);
-        echo "</pre>";
+        Model::sendJSON($this->formatDataLignesAnimaux($lignesAnimal));
+       
         
     }
+
+    private  function formatDataLignesAnimaux($lignes){
+        $tab = [];
+
+        $animaux = $this->apiManager->getDBAnimaux2();
+        $i = 0;
+        foreach ($animaux as $animal){
+            $continents = $this->apiManager->getContinentsAnimal($animal['animal_id']);
+            $animaux[$i]['continents'] = $continents;
+            $i++;
+        }
+        echo "<pre>";
+        print_r($animaux);
+        "</pre>";
+      
+    // Tranformer les data en tableau multidimentionel
+    // $tab = [];
+        
+    // foreach($lignes as $ligne){
+    //     // Ici on récupère un animal avec sa famille et les continents dans lesquels il existe
+    //     if(!array_key_exists($ligne['animal_id'],$tab)){
+    //         $tab[$ligne['animal_id']] = [
+    //             "id" => $ligne['animal_id'],
+    //             "nom" => $ligne['animal_nom'],
+    //             "description" => $ligne['animal_description'],
+    //             "image" => $ligne['animal_image'],
+    //             "famille" => [
+    //                 "idFamille" => $ligne['famille_id'],
+    //                 "libelleFamille" => $ligne['famille_libelle'],
+    //                 "descriptionFamille" => $ligne['famille_description']
+    //             ]
+    //         ];
+    //     }
+           
+    //     $tab[$ligne['animal_id']]['continents'][] = [
+    //         "idContinent" => $ligne['continent_id'],
+    //         "libelleContinent" => $ligne['continent_libelle']
+    //     ];
+    // }
+
+    // return $tab;
+
+  }
 
     public function getContinents(){
         $continents = $this->apiManager->getDBContinents();
